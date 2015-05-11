da22dfead7c57eb60405222f6593dc5ac4704b52
Austin Shindlecker <ashindlecker@gmail.com>
2015-05-11 14:16:27 -0400

    Can type ips to connect to, server list is updated per server received

--- content.js
+++ content.js
@@ -1,11 +1,15 @@
 /// <reference path="typings/jquery/jquery.d.ts"/>
 
-var regions = ["US-Fremont", "US-Atlanta", "BR-Brazil", "EU-London", "RU-Russia", "JP-Tokyo", "CN-China", "SG-Singapore"];
+var regions = [["US-Fremont","US West"], ["US-Atlanta", "US East"], ["BR-Brazil", "South America"], ["EU-London", "Europe"], ["RU-Russia", "Russia"], ["JP-Tokyo", "East Asia"], ["CN-China", "China"], ["SG-Singapore", "Oceana"]];
 var serverIPs = [];
-var retryCount = 50;
+var retryCount = 20;
+
 $("body").ready(function ()
 {
-	createList();
+	createUI();
+	
+	//Get server info
+	
 	for(var i = 0; i < regions.length; i++)
 	{
 		for(var j = 0; j < retryCount; j++)
@@ -14,7 +18,7 @@ $("body").ready(function ()
 			$.ajax({
 				type: "POST",
 				url: "http://m.agar.io/",
-				data: regions[i],
+				data: regions[i][0],
 				tempJ: j,
 				tempI: i,
 				success: function(data)
@@ -22,21 +26,15 @@ $("body").ready(function ()
 					if($.inArray(data.split("\n")[0], serverIPs) == -1)
 					{
 						serverIPs.push(data.split("\n")[0]);
-					}
-					
-					if(this.tempJ == retryCount - 1 && this.tempI == regions.length - 1)
-					{
-						fillList();
-						addMoreUI();
+						var serverListElement = $("#server");
+						var listItem = $("#region").children()[1].cloneNode(true);
+						
+						$(serverListElement.children()[0]).text("-- Select Server --");
+						addServer(serverListElement, listItem, "ws://" + data.split("\n")[0]);
 					}
 				},
 				error: function(data)
 				{
-					if(this.tempJ == retryCount - 1 && this.tempI == regions.length - 1)
-					{
-						fillList();
-						addMoreUI();
-					}
 				}
 			});
 		}
@@ -51,25 +49,89 @@ function addServer(list, toClone, server)
 	list.append(item);
 }
 
-function createList()
+
+function regionChanged(data)
 {
-	var serverListElement = $("#region").clone();
-	var listItemTitle = serverListElement.children()[0].cloneNode(true);
-	listItemTitle.textContent = "-- Loading Server List --";
+	var value = $("#region").val();
+		$.ajax({
+			type: "POST",
+			url: "http://m.agar.io/",
+			data: value,
+			success: function(data)
+			{
+				console.log(data);
+				$("#enterip").val("ws://" + data.split("\n")[0]);
+				connect($("#enterip").val());
+				$("#playBtn").removeAttr("disabled");
+				$("#retryBtn").removeAttr("disabled");
+			},
+			error: function(data)
+			{
+			}
+		});
+}
+
+function createUI()
+{
+	//modify region selection event
+	var itemNode = $("#region").children()[1].cloneNode(true);
+	var titleNode = $("#region").children()[0].cloneNode(true);
+	var parent = $("#region").parent();
+	$("#region").remove();
 	
-	//change id
-	serverListElement.attr("id", "server");
+	var newRegion = $('<select id="region" class="form-control">');
+	
+	newRegion.append(titleNode);
+	for(var i = 0; i < regions.length; i++)
+	{
+		var item = $(itemNode.cloneNode());
+		item.text(regions[i][1]);
+		item.val(regions[i][0]);
+		newRegion.append(item);
+	}
+	
+	newRegion.attr("onchange", "(" + regionChanged + ")()");
+	
+	parent.append(newRegion);
+	
+	//create selection dropdown
+	var serverListElement = $('<select id="server" class="form-control">');
+	var listItemTitle = newRegion.children()[0].cloneNode(true);
+	listItemTitle.textContent = "-- Loading Server List --";
 	//clear clones elements
 	serverListElement.empty();
 	//change functionality
-	serverListElement.attr("onchange", "connect($('#server').val()); $('.needs-ip').removeAttr('disabled');");
+	serverListElement.attr("onchange", "connect($('#server').val()); $('.needs-ip').removeAttr('disabled');$('#playBtn').removeAttr('disabled'); $('#enterip').val($('#server').val());");
 	
 	
 	serverListElement.append(listItemTitle);
 	
 	$("#region").parent().append("</br>");
 	$("#region").parent().append(serverListElement);
+	
+	
+	//create a retry button for IP connecting
+	var uiDiv = $("#playBtn").parent();
+	var retryButton = uiDiv.find("#playBtn").next().clone(false);
+	retryButton.attr("id", "retryBtn").text("Find New Session");
+	
+	retryButton.attr("onclick", "connect($('#enterip').val());");
+	retryButton.attr("disabled", "");
+	retryButton.attr("class", "btn btn-play btn-primary needs-ip");
+	$("#playBtn").parent().append("</br>");
+	$("#playBtn").parent().append(retryButton);
+	$("#playBtn").parent().append("</br>");
+	$("#playBtn").parent().append("</br>");
+	
+	
+	//create a select server text area
+	var typeIp = $("<input id='enterip' class='form-control' placeholder='IP'/>");
+	typeIp.attr("onchange", "connect($('#enterip').val()); $('#playBtn').removeAttr('disabled');$('#retryBtn').removeAttr('disabled');");
+	$("#nick").parent().append("</br>");
+	$("#nick").parent().append(typeIp);
+	
 }
+
 function fillList()
 {
 	var serverListElement = $("#server");
@@ -79,20 +141,4 @@ function fillList()
 	//Fill in title and options
 	for(var i = 0; i < serverIPs.length; i++)
 		addServer(serverListElement, listItem, "ws://" + serverIPs[i]);
-}
-
-function addMoreUI()
-{
-	//create a retry button for IP connecting
-	var uiDiv = $("#playBtn").parent();
-	var retryButton = uiDiv.find("#playBtn").next().clone(false);
-	retryButton.attr("id", "retryBtn").text("Find New Session");
-	
-	retryButton.attr("onclick", "connect($('#server').val());");
-	retryButton.attr("disabled", "");
-	retryButton.attr("class", "btn btn-play btn-primary needs-ip");
-	$("#playBtn").parent().append("</br>");
-	$("#playBtn").parent().append(retryButton);
-	$("#playBtn").parent().append("</br>");
-	$("#playBtn").parent().append("</br>");
 }
